175045dce78fd72e8389641551df9f85651ec79a
Dennis Frijlink <dennisfrijlink@gmail.com>
2025-10-15 20:09:48 +0200

    removed dark themes

--- youtube/__init__.py
+++ youtube/__init__.py
@@ -20,17 +20,10 @@ yt_app.add_url_rule('/settings', 'settings_page', settings.settings_page, method
 def homepage():
     return flask.render_template('home.html', title="Youtube local")
 
-
-theme_names = {
-    0: 'light_theme',
-    1: 'gray_theme',
-    2: 'dark_theme',
-}
-
 @yt_app.context_processor
 def inject_theme_preference():
     return {
-        'theme_path': '/youtube.com/static/' + theme_names[settings.theme] + '.css',
+        'theme_path': '/youtube.com/static/light_theme.css',
         'settings': settings,
     }
 
